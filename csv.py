1a14a3e0f849b01080ee80b8102a9764a3aff88c
Achiel  van der Mandele <achiel@ismoke.lan>
2010-02-10 00:12:33 +0100

    added csv exporter

---
+++ csv.py
@@ -0,0 +1,23 @@
+#!/opt/local/bin/python2.5
+from models import Bet, Matchup
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+
+class MainHandler(webapp.RequestHandler):
+
+	def get(self):
+			w = self.response.out
+			matches = Matchup.all()
+			for m in matches:
+				bets = Bet.all().filter("match = ", m)
+				for b in bets:
+					template = "%s;%s;%s;%s;%s;%s;%s" % (m.team_a_name, m.team_b_name, b.team_a_odds, b.team_b_odds,b.draw_odds,m.date_match_formatted, b.date_recorded)
+					w.write(template)
+					w.write("<br/>")
+
+def main():
+	application = webapp.WSGIApplication([('/csv', MainHandler)], debug=True)
+	util.run_wsgi_app(application)
+
+if __name__ == '__main__':
+	main()
